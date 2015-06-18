78d7a45cbf280465077bd65087450ccc2a2e4f77
SimplyY <SimplyYu@163.com>
2015-06-18 12:35:08 +0800

    增加django的初始文件

---
+++ web_piano/urls.py
@@ -0,0 +1,24 @@
+"""web_piano URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import *
+from web_piano.views import current_datetime
+
+
+# 模式包含了一个尖号(^)和一个美元符号($)。这些都是正则表达式符号，并且有特定的含义：
+# 上箭头要求表达式对字符串的头部进行匹配，美元符号则要求表达式对字符串的尾部进行匹配。
+urlpatterns = patterns('',
+    ('hello/$', current_datetime),
+)
---
+++ web_piano/views.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from django.http import HttpResponse
+import datetime
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
